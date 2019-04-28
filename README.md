7d239fb7cb29f47045782fc1d2e0d7af81598166
KranK <krankshuffle@gmail.com>
2019-04-28 16:24:25 +0300

    Push SourceCode
    Hello Guys !!!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Yu-Gi-Oh-Legacy-of-the-Duelist-Save-Manager
+Yu Gi Oh! Legacy of the Duelist Save Manager with CRC32 Check
+
+![noga_3 5](https://cdn.discordapp.com/attachments/564199590019072001/572039132780560385/unknown.png)
---
+++ Yu_Gi_Oh_Legacy_of_the_Duelist_Save_Manager/Main_Form.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace SaveEditorSwitch
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.openSaveDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Main_Theme_Controller = new MonoFlat.MonoFlat_ThemeContainer();
+            this.MinimizeButton = new MonoFlat.MonoFlat_Button();
+            this.ExitButton = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_Panel2 = new MonoFlat.MonoFlat_Panel();
+            this.monoFlat_SocialButton1 = new MonoFlat.MonoFlat_SocialButton();
+            this.monoFlat_Panel1 = new MonoFlat.MonoFlat_Panel();
+            this.SaveFileSizeLabel = new MonoFlat.MonoFlat_HeaderLabel();
+            this.Checksum_lab = new MonoFlat.MonoFlat_HeaderLabel();
+            this.monoFlat_HeaderLabel1 = new MonoFlat.MonoFlat_HeaderLabel();
+            this.ImportSaveButton = new MonoFlat.MonoFlat_Button();
+            this.MoneyLab = new MonoFlat.MonoFlat_TextBox();
+            this.ExportSaveHandlelab = new MonoFlat.MonoFlat_Panel();
+            this.ExportSaveButton = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_LinkLabel1 = new MonoFlat.MonoFlat_LinkLabel();
+            this.Main_Theme_Controller.SuspendLayout();
+            this.monoFlat_Panel1.SuspendLayout();
+            this.ExportSaveHandlelab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openSaveDialog
+            // 
+            this.openSaveDialog.FileName = "openSaveDialog";
+            // 
+            // Main_Theme_Controller
+            // 
+            this.Main_Theme_Controller.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.Main_Theme_Controller.Controls.Add(this.monoFlat_LinkLabel1);
+            this.Main_Theme_Controller.Controls.Add(this.MinimizeButton);
+            this.Main_Theme_Controller.Controls.Add(this.ExitButton);
+            this.Main_Theme_Controller.Controls.Add(this.monoFlat_Panel2);
+            this.Main_Theme_Controller.Controls.Add(this.monoFlat_SocialButton1);
+            this.Main_Theme_Controller.Controls.Add(this.monoFlat_Panel1);
+            this.Main_Theme_Controller.Controls.Add(this.monoFlat_HeaderLabel1);
+            this.Main_Theme_Controller.Controls.Add(this.ImportSaveButton);
+            this.Main_Theme_Controller.Controls.Add(this.MoneyLab);
+            this.Main_Theme_Controller.Controls.Add(this.ExportSaveHandlelab);
+            this.Main_Theme_Controller.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Main_Theme_Controller.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Main_Theme_Controller.Location = new System.Drawing.Point(0, 0);
+            this.Main_Theme_Controller.Name = "Main_Theme_Controller";
+            this.Main_Theme_Controller.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.Main_Theme_Controller.RoundCorners = true;
+            this.Main_Theme_Controller.Sizable = true;
+            this.Main_Theme_Controller.Size = new System.Drawing.Size(681, 307);
+            this.Main_Theme_Controller.SmartBounds = true;
+            this.Main_Theme_Controller.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.Main_Theme_Controller.TabIndex = 6;
+            this.Main_Theme_Controller.Text = "           Yu-Gi-Oh! Legacy of the Duelist Save Manager";
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.Transparent;
+            this.MinimizeButton.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.MinimizeButton.Image = null;
+            this.MinimizeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.MinimizeButton.Location = new System.Drawing.Point(590, 9);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(38, 41);
+            this.MinimizeButton.TabIndex = 12;
+            this.MinimizeButton.Text = "_";
+            this.MinimizeButton.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Transparent;
+            this.ExitButton.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ExitButton.Image = null;
+            this.ExitButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ExitButton.Location = new System.Drawing.Point(634, 9);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(38, 41);
+            this.ExitButton.TabIndex = 11;
+            this.ExitButton.Text = "X";
+            this.ExitButton.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // monoFlat_Panel2
+            // 
+            this.monoFlat_Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(63)))));
+            this.monoFlat_Panel2.Location = new System.Drawing.Point(588, 7);
+            this.monoFlat_Panel2.Name = "monoFlat_Panel2";
+            this.monoFlat_Panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.monoFlat_Panel2.Size = new System.Drawing.Size(87, 46);
+            this.monoFlat_Panel2.TabIndex = 14;
+            this.monoFlat_Panel2.Text = "monoFlat_Panel2";
+            // 
+            // monoFlat_SocialButton1
+            // 
+            this.monoFlat_SocialButton1.Image = global::Yu_Gi_Oh_Legacy_of_the_Duelist_Save_Manager.Properties.Resources.yugioh_4;
+            this.monoFlat_SocialButton1.Location = new System.Drawing.Point(3, 3);
+            this.monoFlat_SocialButton1.Name = "monoFlat_SocialButton1";
+            this.monoFlat_SocialButton1.Size = new System.Drawing.Size(54, 54);
+            this.monoFlat_SocialButton1.TabIndex = 13;
+            this.monoFlat_SocialButton1.Text = "monoFlat_SocialButton1";
+            // 
+            // monoFlat_Panel1
+            // 
+            this.monoFlat_Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(63)))));
+            this.monoFlat_Panel1.Controls.Add(this.SaveFileSizeLabel);
+            this.monoFlat_Panel1.Controls.Add(this.Checksum_lab);
+            this.monoFlat_Panel1.Location = new System.Drawing.Point(0, 281);
+            this.monoFlat_Panel1.Name = "monoFlat_Panel1";
+            this.monoFlat_Panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.monoFlat_Panel1.Size = new System.Drawing.Size(681, 26);
+            this.monoFlat_Panel1.TabIndex = 10;
+            this.monoFlat_Panel1.Text = "monoFlat_Panel1";
+            // 
+            // SaveFileSizeLabel
+            // 
+            this.SaveFileSizeLabel.AutoSize = true;
+            this.SaveFileSizeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.SaveFileSizeLabel.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.SaveFileSizeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.SaveFileSizeLabel.Location = new System.Drawing.Point(435, 0);
+            this.SaveFileSizeLabel.Name = "SaveFileSizeLabel";
+            this.SaveFileSizeLabel.Size = new System.Drawing.Size(138, 25);
+            this.SaveFileSizeLabel.TabIndex = 10;
+            this.SaveFileSizeLabel.Text = "Save File Size: ";
+            // 
+            // Checksum_lab
+            // 
+            this.Checksum_lab.AutoSize = true;
+            this.Checksum_lab.BackColor = System.Drawing.Color.Transparent;
+            this.Checksum_lab.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.Checksum_lab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Checksum_lab.Location = new System.Drawing.Point(8, 0);
+            this.Checksum_lab.Name = "Checksum_lab";
+            this.Checksum_lab.Size = new System.Drawing.Size(112, 25);
+            this.Checksum_lab.TabIndex = 9;
+            this.Checksum_lab.Text = "Checksum: ";
+            // 
+            // monoFlat_HeaderLabel1
+            // 
+            this.monoFlat_HeaderLabel1.AutoSize = true;
+            this.monoFlat_HeaderLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_HeaderLabel1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.monoFlat_HeaderLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.monoFlat_HeaderLabel1.Location = new System.Drawing.Point(225, 116);
+            this.monoFlat_HeaderLabel1.Name = "monoFlat_HeaderLabel1";
+            this.monoFlat_HeaderLabel1.Size = new System.Drawing.Size(79, 25);
+            this.monoFlat_HeaderLabel1.TabIndex = 8;
+            this.monoFlat_HeaderLabel1.Text = "Money:";
+            // 
+            // ImportSaveButton
+            // 
+            this.ImportSaveButton.BackColor = System.Drawing.Color.Transparent;
+            this.ImportSaveButton.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ImportSaveButton.Image = null;
+            this.ImportSaveButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ImportSaveButton.Location = new System.Drawing.Point(13, 105);
+            this.ImportSaveButton.Name = "ImportSaveButton";
+            this.ImportSaveButton.Size = new System.Drawing.Size(146, 41);
+            this.ImportSaveButton.TabIndex = 6;
+            this.ImportSaveButton.Text = "Import Save";
+            this.ImportSaveButton.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.ImportSaveButton.Click += new System.EventHandler(this.ImportSaveButton_Click_1);
+            // 
+            // MoneyLab
+            // 
+            this.MoneyLab.BackColor = System.Drawing.Color.Transparent;
+            this.MoneyLab.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.MoneyLab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.MoneyLab.Image = null;
+            this.MoneyLab.Location = new System.Drawing.Point(310, 105);
+            this.MoneyLab.MaxLength = 9;
+            this.MoneyLab.Multiline = false;
+            this.MoneyLab.Name = "MoneyLab";
+            this.MoneyLab.ReadOnly = false;
+            this.MoneyLab.Size = new System.Drawing.Size(135, 46);
+            this.MoneyLab.TabIndex = 4;
+            this.MoneyLab.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.MoneyLab.UseSystemPasswordChar = false;
+            // 
+            // ExportSaveHandlelab
+            // 
+            this.ExportSaveHandlelab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(63)))));
+            this.ExportSaveHandlelab.Controls.Add(this.ExportSaveButton);
+            this.ExportSaveHandlelab.Location = new System.Drawing.Point(523, 105);
+            this.ExportSaveHandlelab.Name = "ExportSaveHandlelab";
+            this.ExportSaveHandlelab.Padding = new System.Windows.Forms.Padding(5);
+            this.ExportSaveHandlelab.Size = new System.Drawing.Size(145, 41);
+            this.ExportSaveHandlelab.TabIndex = 15;
+            // 
+            // ExportSaveButton
+            // 
+            this.ExportSaveButton.BackColor = System.Drawing.Color.Transparent;
+            this.ExportSaveButton.Enabled = false;
+            this.ExportSaveButton.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ExportSaveButton.Image = null;
+            this.ExportSaveButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ExportSaveButton.Location = new System.Drawing.Point(0, 0);
+            this.ExportSaveButton.Name = "ExportSaveButton";
+            this.ExportSaveButton.Size = new System.Drawing.Size(146, 41);
+            this.ExportSaveButton.TabIndex = 7;
+            this.ExportSaveButton.Text = "Export Save";
+            this.ExportSaveButton.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.ExportSaveButton.Click += new System.EventHandler(this.ExportSaveButton_Click);
+            // 
+            // monoFlat_LinkLabel1
+            // 
+            this.monoFlat_LinkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.monoFlat_LinkLabel1.AutoSize = true;
+            this.monoFlat_LinkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_LinkLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.monoFlat_LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(41)))), ((int)(((byte)(42)))));
+            this.monoFlat_LinkLabel1.Location = new System.Drawing.Point(275, 247);
+            this.monoFlat_LinkLabel1.Name = "monoFlat_LinkLabel1";
+            this.monoFlat_LinkLabel1.Size = new System.Drawing.Size(124, 20);
+            this.monoFlat_LinkLabel1.TabIndex = 16;
+            this.monoFlat_LinkLabel1.TabStop = true;
+            this.monoFlat_LinkLabel1.Text = "Created By KranK";
+            this.monoFlat_LinkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(41)))), ((int)(((byte)(42)))));
+            this.monoFlat_LinkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.monoFlat_LinkLabel1_LinkClicked);
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 307);
+            this.Controls.Add(this.Main_Theme_Controller);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main_Form";
+            this.Text = "           Yu-Gi-Oh! Legacy of the Duelist Save Manager";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Main_Theme_Controller.ResumeLayout(false);
+            this.Main_Theme_Controller.PerformLayout();
+            this.monoFlat_Panel1.ResumeLayout(false);
+            this.monoFlat_Panel1.PerformLayout();
+            this.ExportSaveHandlelab.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openSaveDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private MonoFlat.MonoFlat_ThemeContainer Main_Theme_Controller;
+        private MonoFlat.MonoFlat_TextBox MoneyLab;
+        private MonoFlat.MonoFlat_Button ImportSaveButton;
+        private MonoFlat.MonoFlat_HeaderLabel Checksum_lab;
+        private MonoFlat.MonoFlat_HeaderLabel monoFlat_HeaderLabel1;
+        private MonoFlat.MonoFlat_Button ExportSaveButton;
+        private MonoFlat.MonoFlat_Button ExitButton;
+        private MonoFlat.MonoFlat_Panel monoFlat_Panel1;
+        private MonoFlat.MonoFlat_Button MinimizeButton;
+        private MonoFlat.MonoFlat_SocialButton monoFlat_SocialButton1;
+        private MonoFlat.MonoFlat_Panel monoFlat_Panel2;
+        private MonoFlat.MonoFlat_Panel ExportSaveHandlelab;
+        private MonoFlat.MonoFlat_HeaderLabel SaveFileSizeLabel;
+        private MonoFlat.MonoFlat_LinkLabel monoFlat_LinkLabel1;
+    }
+}
+
